6d5058bf282da90c4e9cf0b4050fa32baa3e02bd
Raginaakhter <raginaakhter052@gmail.com>
2026-01-14 23:42:33 +0600

    updated demo

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -4,8 +4,7 @@ const Demo = () => {
     return (
         <div>
             Lorem ipsum dolor, sit amet consectetur adipisicing 
-            elit. Veritatis, dolores. Corrupti rem quo illum minus repellendus consequuntur! Animi ullam consequuntur quae culpa tempore repudiandae,
-             nulla, dicta repellendus veniam, totam voluptate.
+          
         </div>
     );
 };
