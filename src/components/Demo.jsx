6aba6aca636f3ab0f8a6de52cd2daf13c8ebc13b
Raginaakhter <raginaakhter052@gmail.com>
2026-01-14 23:40:41 +0600

    add demo

---
+++ src/components/Demo.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Demo = () => {
+    return (
+        <div>
+            Lorem ipsum dolor, sit amet consectetur adipisicing 
+            elit. Veritatis, dolores. Corrupti rem quo illum minus repellendus consequuntur! Animi ullam consequuntur quae culpa tempore repudiandae,
+             nulla, dicta repellendus veniam, totam voluptate.
+        </div>
+    );
+};
+
+export default Demo;
--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,19 +1,19 @@
-// Import the functions you need
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_apiKey,
-  authDomain: import.meta.env.VITE_authDomain,
-  projectId: import.meta.env.VITE_projectId,
-  storageBucket: import.meta.env.VITE_storageBucket,
-  messagingSenderId: import.meta.env.VITE_messagingSenderId,
-  appId: import.meta.env.VITE_appId,
-  measurementId: import.meta.env.VITE_measurementId,
+  apiKey: "AIzaSyD7LokQdaVvPlhARbvGovBBn0PfTCebm58",
+  authDomain: "bhojon-bari-b432f.firebaseapp.com",
+  projectId: "bhojon-bari-b432f",
+  storageBucket: "bhojon-bari-b432f.appspot.com", // ✅ fixed
+  messagingSenderId: "983758424121",
+  appId: "1:983758424121:web:7add5e569f7e8afa2a25e8",
+  measurementId: "G-6XTLND9P4P"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-
-export default app;
+export default app; 
