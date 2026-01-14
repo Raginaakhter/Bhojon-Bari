c5dfda76bf47a6e37647e718c50472beac4de671
Raginaakhter <raginaakhter052@gmail.com>
2026-01-14 23:51:57 +0600

    delet text

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -4,8 +4,8 @@ const Demo = () => {
     return (
         <div>
            
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Modi deleniti possimus aut perferendis vel iusto vitae, rem, a minus sunt voluptatibus adipisci 
-fg Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus quia maxime ad, repellat deserunt corporis excepturi, nihil temporibus consectetur vel delectus tempore soluta vitae mollitia aperiam incidunt? Nulla, tempora porro.
+
+fg Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus quia maxime ad, repellat deserunt corporis excepturi, nihil temporibus consectetur vel delectus tempore soluta 
         </div>
     );
 };
